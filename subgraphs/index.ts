57ee38b6a64b38cf0b4910a4c63a7f6678a1ed4a
Frank <frank.pfeift@protonmail.com>
2026-01-20 13:10:46 +0100

    chore: sync powerhouse dependencies to 6.0.0-dev.12
    - Updated to powerhouse 6.0.0-dev.12 (dev channel)
    - Bumped version to 0.0.6-dev.0
    - Updated workflow with semantic-release support
    - Removed renown-user subgraph (merged into main)

--- subgraphs/index.ts
+++ subgraphs/index.ts
@@ -1,4 +1,4 @@
 export {};
-export * as RenownUserSubgraph from "./renown-user/index.js";
+// export * as RenownUserSubgraph from "./renown-user/index.js";
 export * as RenownReadModelSubgraph from "./renown-read-model/index.js";
 export * as RenownCredentialSubgraph from "./renown-credential/index.js";
